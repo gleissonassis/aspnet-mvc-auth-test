d248a74285a5573aeee060ca9afb1578fd8f7e17
Gleisson de Assis <gleisson.assis@gmail.com>
2018-04-23 16:59:12 -0300

    Removing uncessary files

--- CP01/CP01/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Inherits="CP01.Global" %>
--- CP01/CP01/Models/Fabricante.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace CP01.Models
-{
-    public class Fabricante
-    {
-        public long FabricanteId { get; set; }
-        public string Nome { get; set; }
-    }
-}
